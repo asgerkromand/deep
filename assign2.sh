549ec7f8195626b1945fa0db9388c8cee4a90da1
Asger Kromand <crm406@sodas.ku.dk>
2024-10-04 16:19:40 +0200

    commit_7

--- assign2.sh
+++ assign2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=fine_tune_star_wars # Job name
 #SBATCH --output=results_star_wars.out # Name of output file
-#SBATCH --cpus-per-task=1 # Schedule one core
+#SBATCH --cpus-per-task=2 # Schedule one core
 #SBATCH --time=01:00:00 # Run time (hh:mm:ss)
 #SBATCH --gres=gpu
 #SBATCH --mail-type=BEGIN,END,FAIL # E-mail when status changes
