e2307f76124bf6629f3b8fd6b724248578e83c00
Asger Kromand <crm406@sodas.ku.dk>
2024-10-04 16:38:19 +0200

    commit_11

--- assign2.sh
+++ assign2.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=results_star_wars.out # Name of output file
 #SBATCH --cpus-per-task=2 # Schedule one core
 #SBATCH --time=01:00:00 # Run time (hh:mm:ss)
-#SBATCH --gres=gpu
+#SBATCH --gres=gpu:2
 #SBATCH --mail-type=BEGIN,END,FAIL # E-mail when status changes
 #SBATCH --partition=brown
 
