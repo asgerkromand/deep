1b4fefb4dc5d3ade25f8e3e747cc37d1c0dd10dd
Asger Kromand <crm406@sodas.ku.dk>
2024-10-25 14:42:55 +0200

    assignment_4

--- assign_4/assign4.sh
+++ assign_4/assign4.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=fine_tune_star_wars # Job name
-#SBATCH --output=results_star_wars.out # Name of output file
+#SBATCH --job-name=assignment_4 # Job name
+#SBATCH --output=assignment4_2.out # Name of output file
 #SBATCH --cpus-per-task=2 # Schedule one core
 #SBATCH --time=01:00:00 # Run time (hh:mm:ss)
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu
 #SBATCH --mail-type=BEGIN,END,FAIL # E-mail when status changes
 #SBATCH --partition=brown
 
 module load Anaconda3
 source activate /opt/itu/condaenv/cs/rapu/synth_transformers
+
+cd induction_heads_assignment  
+python train_transformer.py --n_epoch=25  
+python run_transformer.py  
